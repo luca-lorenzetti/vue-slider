46d233f04833dd05677eafde20a36e11455c26ce
luca <luca.lorenzetti0@gmail.com>
2021-03-25 14:54:07 +0100

    Inserito funzionamento avanti e indietro
    mancano ancora il funzionamento sui bullets

--- js/script.js
+++ js/script.js
@@ -6,11 +6,19 @@ var app = new Vue({
         "https://static.photocdn.pt/images/articles/2017/04/28/iStock-646511634.jpg",
         "https://cdn.mos.cms.futurecdn.net/FUE7XiFApEqWZQ85wYcAfM.jpg",
         "https://static.photocdn.pt/images/articles/2017/04/28/iStock-546424192.jpg"
-      ]
+      ],
+      currentPos: 0
     },
     methods: {
-      saluto: function(){
-        alert("CIao")
+      nextImage: function(elem, index){
+        
+        if( this.currentPos < 0){
+          this.currentPos = this.imagesUrl.length-1;
+        }
+        else if( this.currentPos > this.imagesUrl.length-1){
+          this.currentPos = 0;
+        }
+
       }
     },
   })
